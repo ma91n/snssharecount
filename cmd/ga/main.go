4f59f0f0279ae5f45c4d57b57b2601171c2f6a78
future-mano <j.mano.fg@future.co.jp>
2023-11-04 15:16:55 +0900

    Support GA4

--- cmd/ga/main.go
+++ cmd/ga/main.go
@@ -5,7 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/urfave/cli"
-	"google.golang.org/api/analyticsreporting/v4"
+	ga "google.golang.org/api/analyticsdata/v1beta"
+	//"google.golang.org/api/analyticsreporting/v4"
 	"google.golang.org/api/option"
 	"log"
 	"os"
@@ -26,26 +27,26 @@ type GoogleAnalyticsPV struct {
 func main() {
 
 	app := &cli.App{
-		Name:  "sns share count",
-		Usage: "snssharecount <URL>",
+		Name:  "google analytics",
+		Usage: "ga",
 		Action: func(cCtx *cli.Context) error {
 			ctx := context.Background()
 
 			// https://pkg.go.dev/google.golang.org/api/analyticsreporting/v4
-			ars, err := analyticsreporting.NewService(ctx, option.WithScopes(analyticsreporting.AnalyticsReadonlyScope))
+			ars, err := ga.NewService(ctx, option.WithScopes(ga.AnalyticsReadonlyScope))
 			if err != nil {
 				return fmt.Errorf("new google analytice service: %w", err)
 			}
 
-			pvWeekly, err := fetchGoogleAnalytics(ars, ctx, "7daysAgo", "1daysAgo")
+			pvWeekly, err := fetchGoogleAnalytics(ars, ctx, "7daysAgo", "today")
 			if err != nil {
 				return err
 			}
-			pvMonthly, err := fetchGoogleAnalytics(ars, ctx, "30daysAgo", "1daysAgo")
+			pvMonthly, err := fetchGoogleAnalytics(ars, ctx, "30daysAgo", "today")
 			if err != nil {
 				return err
 			}
-			pvYearly, err := fetchGoogleAnalytics(ars, ctx, "365daysAgo", "1daysAgo")
+			pvYearly, err := fetchGoogleAnalytics(ars, ctx, "365daysAgo", "today")
 			if err != nil {
 				return err
 			}
@@ -73,27 +74,18 @@ func main() {
 
 }
 
-func fetchGoogleAnalytics(ars *analyticsreporting.Service, ctx context.Context, start, end string) ([]GoogleAnalyticsPV, error) {
-	resp, err := ars.Reports.BatchGet(&analyticsreporting.GetReportsRequest{
-		ReportRequests: []*analyticsreporting.ReportRequest{{
-			DateRanges: []*analyticsreporting.DateRange{
-				{StartDate: start, EndDate: end}},
-			Dimensions: []*analyticsreporting.Dimension{
-				{Name: "ga:pagePath"},
-				{Name: "ga:pageTitle"},
+func fetchGoogleAnalytics(ars *ga.Service, ctx context.Context, start, end string) ([]GoogleAnalyticsPV, error) {
+	resp, err := ars.Properties.BatchRunReports("properties/319098367", &ga.BatchRunReportsRequest{
+		Requests: []*ga.RunReportRequest{
+			{
+				Property:   "properties/319098367",
+				DateRanges: []*ga.DateRange{{StartDate: start, EndDate: end}},
+				Dimensions: []*ga.Dimension{{Name: "pagePath"}, {Name: "pageTitle"}},
+				Metrics:    []*ga.Metric{{Name: "screenPageViews"}},
+				OrderBys:   []*ga.OrderBy{{Desc: true, Dimension: &ga.DimensionOrderBy{DimensionName: "screenPageViews"}}},
+				Limit:      50,
 			},
-			Metrics: []*analyticsreporting.Metric{
-				{Expression: "ga:pageviews"},
-			},
-			OrderBys: []*analyticsreporting.OrderBy{
-				{
-					FieldName: "ga:pageviews",
-					SortOrder: "DESCENDING",
-				},
-			},
-			PageSize: 50,
-			ViewId:   "117039269",
-		}},
+		},
 	}).Context(ctx).Do()
 	if err != nil {
 		return nil, fmt.Errorf("reporting batch get: %w", err)
@@ -101,11 +93,11 @@ func fetchGoogleAnalytics(ars *analyticsreporting.Service, ctx context.Context, 
 
 	pvs := make([]GoogleAnalyticsPV, 0, 50)
 	for _, report := range resp.Reports {
-		for _, row := range report.Data.Rows {
+		for _, row := range report.Rows {
 			pvs = append(pvs, GoogleAnalyticsPV{
-				Path:  row.Dimensions[0],
-				Pv:    row.Metrics[0].Values[0],
-				Title: row.Dimensions[1],
+				Path:  row.DimensionValues[0].Value,
+				Pv:    row.MetricValues[0].Value,
+				Title: row.DimensionValues[1].Value,
 			})
 		}
 	}
